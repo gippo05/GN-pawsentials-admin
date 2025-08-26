7406aa3401cf4d07cc18fe0e8afe0d45e2ac0661
gippo05 <gsarinas17@gmail.com>
2025-08-27 03:41:42 +0800

    utilized api endpoints api.js

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "@/config/config";
+import API from "@/utils/api";
 
 
 export default function AdminLogin() {
@@ -12,7 +12,7 @@ export default function AdminLogin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${API_URL}/api/admin/login`, {
+      const res = await axios.post(`${API.ADMINLOGIN}`, {
         username,
         password,
       });
--- src/utils/api.js
+++ src/utils/api.js
@@ -9,7 +9,8 @@ const API = {
       PRODUCTS: `${BASE_URL}/api/products`,
       TOTALORDERS: `${BASE_URL}/api/dashboard`,
       TOTALREVENUE: `${BASE_URL}/api/dashboard/totalsales`,
-      ORDERS: `${BASE_URL}/api/orders`
+      ORDERS: `${BASE_URL}/api/orders`,
+      ADMINLOGIN: `${BASE_URL}/api/admin/login`
 }
 
 export default API;
